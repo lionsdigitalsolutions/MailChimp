5b11d733baa4630cc9bd2aaf59d8e221259ec625
amirsssss <amir_matics2008@yahoo.com>
2020-12-14 02:32:47 +0330

    first commit

---
+++ src/marketing/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import MarketingPreference
+
+class MarketingPreferenceAdmin(admin.ModelAdmin):
+    list_display = ['__str__','subscribed','update']
+    readonly_fields=['mailchimp_msg','mailchimp_subscribed','timestamp','update']
+    class Meta:
+        model = MarketingPreference
+        fields= [
+            'user',
+            'subscribed',
+            'mailchimp_msg' ,
+            'mailchimp_subscribed',
+            'timestamp',
+            'update'
+            ]
+            
+admin.site.register(MarketingPreference,MarketingPreferenceAdmin)
+
